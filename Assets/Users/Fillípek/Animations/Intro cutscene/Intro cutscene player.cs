f7e94479e8fb4f4609fd9d0627d1292770586abe
Catmaster543 <mastercat543@gmail.com>
2026-02-28 22:47:09 +0100

    a whole lot of changes

--- Assets/Users/Fillípek/Animations/Intro cutscene/Intro cutscene player.cs
+++ Assets/Users/Fillípek/Animations/Intro cutscene/Intro cutscene player.cs
@@ -3,8 +3,8 @@ using TMPro;
 using UnityEngine.UI;
 
 
-public class Dialogue : MonoBehaviour
-{   
+public class IntroCutscenePlayer : MonoBehaviour
+{
     public TextMeshProUGUI textComponent;
     [SerializeField] private Image portraitImage;
     public Sprite[] portraits;
@@ -12,18 +12,23 @@ public class Dialogue : MonoBehaviour
     private float textSpeed = 0.05f;
     private int index;
 
+    public Animator animator;
+
+    public int line_count;
+
     void Start()
     {
         textComponent.text = string.Empty;
         UpdatePortrait();
         StartDialogue();
-        
+
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.KeypadEnter));
         {
             NextLine();
+            line_count++;
         }
     }
     void StartDialogue()
@@ -40,21 +45,27 @@ public class Dialogue : MonoBehaviour
             yield return new WaitForSeconds(textSpeed);
         }
     }
+
     public void NextLine()
     {
         if (index < lines.Length - 1)
         {
+            if (line_count == 2)
+            {
+                animator.SetBool("Show picture", true);
+            }
             index++;
             textComponent.text = string.Empty;
             UpdatePortrait();
             StartCoroutine(TypeLine());
-            
+
         }
         else
         {
             textComponent.text = string.Empty;
         }
     }
+
     public void UpdatePortrait()
     {
         if (portraitImage == null) return;
